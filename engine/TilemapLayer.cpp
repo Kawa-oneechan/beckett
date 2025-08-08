52b069e3cec32234d28ddb68fbd53d9eaeb24d53
Kawa <kawaoneechan@gmail.com>
2025-08-08 14:05:36 +0200

    Add tilemaps (WIP)

---
+++ engine/TilemapLayer.cpp
@@ -0,0 +1,112 @@
+#include "TilemapLayer.h"
+#include "TextUtils.h"
+#include "SpriteRenderer.h"
+#include "../support/format.h"
+#include "../Game.h"
+
+extern float scale;
+
+TilemapLayer::TilemapLayer(jsonValue& doc, const std::string& source, const std::string& layer)
+{
+	auto obj = doc.as_object();
+	for (auto& l : obj["layers"].as_array())
+	{
+		auto lo = l.as_object();
+		if (lo["name"].as_string() == layer)
+		{
+			layerName = layer;
+			width = lo["width"].as_integer();
+			height = lo["width"].as_integer();
+			
+			auto totalSize = this->width * this->height;
+
+			data = std::make_unique<int[]>(totalSize);
+			int i = 0;
+			for (auto& di : lo["data"].as_array())
+			{
+				data[i++] = di.as_integer();
+			}
+			break;
+		}
+	}
+
+	if (data == nullptr)
+		throw std::exception("Couldn't find specified layer.");
+
+	//Now handle the tileset.
+	//We only support the one, and it has to be "from sheet".
+	//TODO: support external tileset files.
+	auto ts = obj["tilesets"].as_array()[0].as_object();
+	{
+		tileWidth = ts["tilewidth"].as_integer();
+		tileHeight= ts["tileheight"].as_integer();
+
+		auto src = ts["image"].as_string();
+		std::string here;
+		auto slashPos = source.rfind('/');
+		if (slashPos != std::string::npos)
+			here = source.substr(0, slashPos + 1);
+		auto tsp = here + src;
+		tsp = ResolvePath(tsp);
+		tilesetTexture = std::make_shared<Texture>(tsp);
+
+		tilesPerLine = tilesetTexture->width / tileWidth;
+	}
+}
+
+void TilemapLayer::Draw(float dt)
+{
+	dt;
+	if (!data)
+		return;
+
+	int i = 0;
+	float s = Scale > 0 ? Scale : scale;
+	
+	const auto tileSize = glm::vec2(tileWidth, tileHeight) * s;
+	for (auto row = 0; row < height; row++)
+	{
+		for (auto col = 0; col < width; col++)
+		{
+			auto tile = data[i++];
+			if (tile == 0)
+				continue;
+			tile--;
+
+			auto srcX = (tile % tilesPerLine) * tileWidth;
+			auto srcY = (tile / tilesPerLine) * tileHeight;
+			auto srcRect = glm::vec4(srcX, srcY, tileWidth, tileHeight);
+
+			Sprite::DrawSprite(*tilesetTexture, glm::vec2(col * tileWidth, row * tileHeight) * s, tileSize, srcRect);
+		}
+	}
+}
+
+TilemapManager::TilemapManager(const std::string& source)
+{
+	auto doc = VFS::ReadJSON(source);
+	auto obj = doc.as_object();
+	for (auto& l : obj["layers"].as_array())
+	{
+		auto lo = l.as_object();
+		auto ln = lo["name"].as_string();
+		layers.emplace_back(doc, source, ln);
+	}
+}
+
+bool TilemapManager::Tick(float dt)
+{
+	dt;
+	for (auto& l : layers)
+	{
+		l.Scale = Scale;
+		//l.Camera = Camera;
+	}
+	return true;
+}
+
+TilemapLayer& TilemapManager::operator[](size_t i)
+{
+	if (i < layers.size()) return layers[i];
+	return layers[0];
+}
---
+++ engine/TilemapLayer.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Tickable.h"
+#include "Texture.h"
+#include "JsonUtils.h"
+
+class TilemapLayer : public Tickable
+{
+private:
+	std::shared_ptr<Texture> tilesetTexture{ nullptr };
+	int width{ 0 };
+	int height{ 0 };
+	int tileWidth{ 0 };
+	int tileHeight{ 0 };
+	int tilesPerLine{ 0 };
+	std::string layerName;
+	std::unique_ptr<int[]> data{ nullptr };
+
+public:
+	TilemapLayer() = default;
+	TilemapLayer(jsonValue& doc, const std::string& source, const std::string& layer);
+	void Draw(float dt);
+
+	float Scale{ -1.0f };
+};
+
+using TilemapLayerP = std::shared_ptr<TilemapLayer>;
+
+//Provides as easier means to load a multi-layer map.
+//To properly use, place this somewhere in the tickable tree, then add the component layers
+//to the tree in their proper order so you can render sprites and such inbetween.
+class TilemapManager : public Tickable
+{
+private:
+	std::vector<TilemapLayer> layers;
+
+public:
+	glm::vec2 Camera;
+	float Scale{ -1.0f };
+
+	TilemapManager(const std::string& source);
+	void Draw(float dt) {}
+	bool Tick(float);
+	
+	TilemapLayer& operator[](size_t i);
+};
