555443b4ac6eda83d159f5bb537d378b917b4e2c
Kawa <kawaoneechan@gmail.com>
2025-11-22 16:27:03 +0100

    Update SpriteRenderer.cpp
    Such a tiny change but yeah

--- engine/SpriteRenderer.cpp
+++ engine/SpriteRenderer.cpp
@@ -334,7 +334,7 @@ namespace Sprite
 		unsigned int fontID;
 		glGenTextures(1, &fontID);
 		glBindTexture(GL_TEXTURE_2D, fontID);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, FontAtlasExtent, FontAtlasExtent, 0, GL_RED, GL_UNSIGNED_BYTE, ttfBitmap);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, FontAtlasExtent, FontAtlasExtent, 0, GL_RED, GL_UNSIGNED_BYTE, ttfBitmap);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
