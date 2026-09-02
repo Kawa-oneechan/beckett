22cc64bff9c427d124a00caaa0a206429410c792
Kawa <kawaoneechan@gmail.com>
2026-09-02 23:00:41 +0200

    Scriptable: use proxy class to give `[]` back

--- Scriptable.h
+++ Scriptable.h
@@ -3,6 +3,32 @@
 #include "engine/Scripting.h"
 #include <sol.hpp>
 
+namespace Scripting
+{
+	extern sol::state* __sol;
+
+	class SolProxy
+	{
+	public:
+		template <typename T>
+		sol::proxy<sol::global_table&, T> operator[](T&& key)
+		{
+			return __sol->operator[](std::forward<T>(key));
+		}
+
+		sol::unsafe_function_result script(const sol::string_view& code, const std::string& chunkname = sol::detail::default_chunk_name(), sol::load_mode mode = sol::load_mode::any)
+		{
+			return __sol->unsafe_script(code, chunkname, mode);
+		}
+
+		sol::protected_function_result do_string(const sol::string_view& code, const std::string& chunkname = sol::detail::default_chunk_name(), sol::load_mode mode = sol::load_mode::any)
+		{
+			return __sol->do_string(code, chunkname, mode);
+		}
+	};
+}
+
+
 class Scriptable
 {
 private:
